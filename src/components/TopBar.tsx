858a76eeb970cb412ec2e74caeaad9e94ce36d26
Kevin Misigaro <kevin@databalk.nu>
2024-08-21 11:24:04 +0200

    refactoring

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@ import { CiMapPin, CiPhone, CiMail } from "react-icons/ci";
 function TopBar() {
   return (
     <div className="py-3 bg-gray-600 flex flex-row justify-between text-white px-5 md:px-10">
-      <div className="flex flex-row items-center gap-x-10">
+      <div className="flex flex-col md:flex-row items-center gap-x-10">
         <div className="flex flex-row items-center gap-x-3">
           <CiMapPin className="text-xl" />
           <div className="text-sm">Location</div>
@@ -14,7 +14,7 @@ function TopBar() {
           <div className="text-sm">Phone</div>
         </div>
       </div>
-      <div className="flex flex-row items-center gap-x-10">
+      <div className="flex flex-col md:flex-row items-center gap-x-10">
         <div className="flex flex-row items-center gap-x-3">
           <CiMail />
           <div className="text-sm">Email</div>
