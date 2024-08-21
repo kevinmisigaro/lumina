8e6bd22e852276417bc7c06aa9c31a486304ddd3
Kevin Misigaro <kevin@databalk.nu>
2024-08-21 11:29:14 +0200

    change font from Inter to Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({ subsets: ["latin"], weight: "500" })
 
 export const metadata: Metadata = {
   title: "Lumina Ventues",
